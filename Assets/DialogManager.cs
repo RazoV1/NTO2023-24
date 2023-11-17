c9416616f1f6a63bbda1e178276c85a543101320
arboos <151074202+arboos@users.noreply.github.com>
2023-11-17 23:42:05 +0300

    final changes

--- Assets/DialogManager.cs
+++ Assets/DialogManager.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using TMPro;
+using UnityEditor.Build;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
@@ -8,26 +12,72 @@ using UnityEngine.Timeline;
 public class DialogManager : MonoBehaviour
 {
 
-    public GameObject Speaker;
+    public Animator SpeakerAnimator;
     public GameObject Holo;
+    public TextMeshProUGUI DialogText;
     private DialogManager instance = null;
+
+    public string linkJson;
+
+    [SerializeField] private TextAsset jsonFile;
+    [SerializeField] private JsonObjects jsonObj;
+
+    private List<string> phrasesList;
+    private int currentPhrase = 0;
+    private bool textRunning;
+
+    //Эта переменная нужна для того, чтобы мы могли менять автоматическое определение 
+    //анимации (в функции Update), на ручное в таймлайне
+    public bool customSpeakerAnim;
+    
     void Start () {
         if (instance == null) { 
             instance = this; 
         } else if(instance == this){
             Destroy(gameObject);
         }
+
+        //Парсим json файл dialogA(jsonFile) в массив phrasesList 
+        phrasesList = new List<string>();
+        linkJson = Application.persistentDataPath + "Assets/dialogA.json";
+        jsonObj = JsonUtility.FromJson<JsonObjects>(jsonFile.text);
+        phrasesList = jsonObj.phrases;
+        
+        //Инициализируем компоненты
+        SpeakerAnimator = SpeakerAnimator.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        //Автоматическое определение нужной анимации говорящего
+        if (textRunning) SpeakerAnimator.SetTrigger("speak");
+        else SpeakerAnimator.SetTrigger("idle");
     }
     
-    public void AnimationSpeakerTalk()
+    //При помощи этой функции запускаем корутину с выводом текста
+    public void LoadText()
     {
-        //Запускает анимацию разговора по триггеру
-        Speaker.GetComponent<Animator>().SetTrigger("speak");
+        if (phrasesList.Count > currentPhrase)
+        {
+            StartCoroutine(RunText(3f));
+            currentPhrase++;
+        }
     }
     
-    public void AnimationSpeakerIdle()
+    public IEnumerator RunText(float pauseBetweenPhrases)
     {
-        //Запускает анимацию разговора по триггеру
-        Speaker.GetComponent<Animator>().SetTrigger("idle");
+        foreach (var text in phrasesList)
+        {
+            foreach (var letter in text)
+            {
+                textRunning = true;
+                DialogText.text += letter;
+                yield return new WaitForSeconds(0.05f);
+            }
+
+            textRunning = false;
+            yield return new WaitForSeconds(pauseBetweenPhrases);
+            DialogText.text = "";
+        }
     }
 }
---
+++ Assets/JsonObjects.cs
@@ -0,0 +1,8 @@
+
+using System.Collections.Generic;
+
+[System.Serializable]
+public struct JsonObjects
+{
+    public List<string> phrases;
+}
