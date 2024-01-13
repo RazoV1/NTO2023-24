df90ee8c411e288e1c410ecd30b63d806131e8d0
arboos <151074202+arboos@users.noreply.github.com>
2024-01-13 14:33:10 +0300

    main menu bux fix + volume change settings

---
+++ Assets/_Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider masterSlider;
+
+    private void Start()
+    {
+        audioMixer = audioMixer.GetComponent<AudioMixer>();
+        
+    }
+
+    public void OnMusicSliderChange()
+    {
+        audioMixer.SetFloat("MusicVolume", -80 + musicSlider.value * 100);
+        audioMixer.SetFloat("MasterVolume", -80 + masterSlider.value * 100);
+    }
+}
