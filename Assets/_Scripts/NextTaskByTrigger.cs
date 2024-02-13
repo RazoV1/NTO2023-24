5ff2515860514f8275dcd43616e66cba0856c794
arboos <151074202+arboos@users.noreply.github.com>
2024-02-14 00:23:13 +0300

    29, 30 task created

---
+++ Assets/_Scripts/NextTaskByTrigger.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextTaskByTrigger : MonoBehaviour
+{
+    [SerializeField] private bool isLore;
+    [SerializeField] private int loreTask;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (isLore)
+            {
+                if (loreTask <= Camera.main.GetComponent<TaskbarManager>().currentTask)
+                {
+                    Camera.main.GetComponent<TaskbarManager>().NextTask();
+                }
+            }
+            else
+            {
+                Camera.main.GetComponent<TaskbarManager>().NextTask();
+            }
+        }
+    }
+}
